bf98ed82a5cb2c188a3b06271d7771cfdb8d199b
rlsalcido24 <rsalcido@alumni.stanford.edu>
2023-05-13 12:26:29 -0700

    Update assert_to_array.sql

--- tests/assert_to_array.sql
+++ tests/assert_to_array.sql
@@ -2,12 +2,12 @@ with test as (
     
     select
     input,
-    split(input, ",") as expected_output
-    ,{{to_array('input')}} as actual_output
+    expected_output,
+    {{to_array('input')}} as actual_output
 from {{ ref('springbrickstests')}}
 where function_name = 'to_array'
 )
 
 select *
 from test
-where expected_output <> actual_output
+where expected_output <> actual_output::string
