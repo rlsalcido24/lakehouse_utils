0810b0a0714344077cb2b1712c939ebf57402af2
rlsalcido24 <rsalcido@alumni.stanford.edu>
2023-05-13 12:37:50 -0700

    Update assert_startswith.sql

--- tests/assert_startswith.sql
+++ tests/assert_startswith.sql
@@ -1,8 +1,9 @@
 with test as (
     
     select
-    *
-    ,{{startswith('input','"te"')}} as actual_output
+    input,
+    expected_output,
+    {{startswith('input','"te"')}} as actual_output
 from {{ ref('springbrickstests')}}
 where function_name = 'startswith'
      
