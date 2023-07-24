bfd4581beb7f337f24683ac85ff3cb9a14325e58
rlsalcido24 <rsalcido@alumni.stanford.edu>
2023-07-24 13:56:39 -0700

    Add files via upload

---
+++ tmp/customerdb.sql
@@ -0,0 +1,25 @@
+
+{{
+    config(
+        materialized = 'table',
+        tag = 'dbx'
+    )
+}}
+
+
+select 
+    {{ zeroifnull('c_custkey') }} as intkey,
+    {{ contains('c_name','"customer"') }} as namecontains,
+    c_address,
+    c_nationkey,
+    c_phone, 
+    {{ to_number ('c_acctbal', 10, 5)}} as cleanacctbal,
+    c_mktsegment,
+    c_comment
+
+
+
+
+from
+    samples.tpch.customer
+    
