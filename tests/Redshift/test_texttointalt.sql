d664b321b8457f1f66c34647e04aa3757ddbed59
rlsalcido24 <rsalcido@alumni.stanford.edu>
2023-09-06 11:36:49 -0700

    Delete tests/Redshift/test_texttointalt.sql

--- tests/Redshift/test_texttointalt.sql
+++
@@ -1,8 +0,0 @@
--- tests/test_texttointalt.sql
-
-SELECT 
-    input_expression,
-    format_phrase,
-    {{ texttointalt('input_expression', 'format_phrase') }} AS result
-FROM
-    dbt.dbt_schema.test3
