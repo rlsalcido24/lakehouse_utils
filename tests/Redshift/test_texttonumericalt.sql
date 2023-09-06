349f37028dd98dcb6c95d32b1fed0ca3f99aab01
rlsalcido24 <rsalcido@alumni.stanford.edu>
2023-09-06 11:37:30 -0700

    Update test_texttonumericalt.sql

--- tests/Redshift/test_texttonumericalt.sql
+++ tests/Redshift/test_texttonumericalt.sql
@@ -1,9 +1,21 @@
--- tests/test_texttonumericalt.sql
+with test as (
+    
+    select
+    '1.5' as inputuno,
+    '2' as snowoutputuno,
+    {{texttonumericalt('inputuno')}} as dbxoutputuno,
+    '2.51' as inputdos,
+    '3' as snowoutputdos,
+    {{texttonumericalt('inputdos')}} as dbxoutputdos,
+    '123.52501' as inputtres,
+    '123.53' as snowoutputtres,
+    {{texttonumericalt('inputtres', 10, 2)}} as dbxoutputtres
 
-WITH test AS (SELECT 
-    123.5201 as testnum,
-    10 as precision,
-    2 as scale
+
 )
 
-SELECT ROUND(CAST(testnum AS DECIMAL(PRECISION, scale)), scale) FROM test;
+select *
+from test
+where (snowoutputuno <> dbxoutputuno
+	or snowoutputdos <> dbxoutputdos 
+	or snowoutputtres <> dbxoutputtres)
