111f1ac95e5b2e224ebc908f7e8468fef86385c4
rlsalcido24 <rlsalcido24>
2024-01-25 18:51:59 +0000

    regexit updates
    regexit updates

--- integration_tests/regexit.py
+++ integration_tests/regexit.py
@@ -1,5 +1,5 @@
 import os
-os.chdir("/Workspace/Repos/roberto.salcido@databricks.com/lakehouse_utils/integration_tests")
+os.chdir("lakehouse_utils/integration_tests")
 syntaxakpath = 'syntaxak.sql' 
 #functionakpath = 'functionak.sql' 
 pyakpath = 'tresak.py' 
@@ -17,13 +17,13 @@ with open(lookerakpath, 'r+') as file:
 with open(pyakpath, 'r+') as file:
     pyak = file.read()
 
-os.chdir("/Workspace/Repos/roberto.salcido@databricks.com/lakehouse_utils/models/redshift_to_databricks") 
+os.chdir("lakehouse_utils/models/redshift_to_databricks") 
 syntaxgenpath = 'customerrs.sql'
 
 with open(syntaxgenpath, 'r+') as file:
     syntaxgen = file.read()
 
-os.chdir("/Workspace/Repos/roberto.salcido@databricks.com/lakehouse_utils/beyondsqltest")
+os.chdir("lakehouse_utils/beyondsqltest")
 pygenpath = "testpyfiles_to_databricks/tres.py"
 lookergenpath = "testlookmlfiles_to_databricks/uno.lkml"   
 
@@ -33,12 +33,9 @@ with open(pygenpath, 'r+') as file:
 with open(lookergenpath, 'r+') as file:
     lookergen = file.read()
 
-#print(syntaxgen)
-#print(syntaxak)
+
 assert syntaxak == syntaxgen
 assert pyak == pygen
-#print(pygen)
-#print(pyak)
 assert lookerak == lookergen
 
 print('wooho, test passed!!')
